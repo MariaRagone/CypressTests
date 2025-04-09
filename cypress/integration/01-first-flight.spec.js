69608f1ad7e290cc35d8b955ffdcbf05764e37b1
mragone <maria.ragone@samaritanministries.org>
2025-04-09 13:57:48 -0400

    added cypress studio test on jetsetter form

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -19,4 +19,19 @@ describe('Create a New Item', () => {
   it('should type in an input field', () => {
     cy.get('[data-test="new-item-input"]').type('T-shirt');
   });
+
+  /* ==== Test Created with Cypress Studio ==== */
+  it('UserFlow', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.get('[data-test="new-item-input"]').clear();
+    cy.get('[data-test="new-item-input"]').type('Phone');
+    cy.get('[data-test="add-item"]').click();
+    cy.get('[data-test="filter-items"]').clear();
+    cy.get('[data-test="filter-items"]').type('phone');
+    cy.get('#item-6').check();
+    cy.get('#item-6').uncheck();
+    cy.get('[data-test="mark-all-as-unpacked"]').click();
+    cy.get('[data-test="remove-all"]').click();
+    /* ==== End Cypress Studio ==== */
+  });
 });
